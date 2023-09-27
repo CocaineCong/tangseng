e78986bae282e32bf80e70b939efe1db4523f0d6
starry <1515770217@qq.com>
2023-09-27 21:36:09 +0800

    改用并行计算bm25，完善测试

--- pkg/util/relevant/bm25direct.go
+++ pkg/util/relevant/bm25direct.go
@@ -1,7 +1,9 @@
-package tfbm
+package relevant
 
 import (
+	"sort"
 	"strings"
+	"sync"
 )
 
 // 计算词项频率（Term Frequency）
@@ -28,21 +30,19 @@ func calculateIDF(term string, documents []string) float64 {
 }
 
 // 计算 BM25
-func CalculateBM25(term string, document string, documents []string, k1 float64, b float64) float64 {
-	tf := calculateTF(term, document)                  // 计算词项频率
-	idf := calculateIDF(term, documents)               // 计算逆文档频率
-	doclen := float64(len(strings.Fields(document)))   // 文档长度（单词数）
-	avgdl := calculateAverageDocumentLength(documents) // 计算平均文档长度
-	corpusSize := float64(len(documents))              // 文档集合大小
+func calculateBM25(term string, document string, documents []string, avgdl, k1 float64, b float64) float64 {
+	tf := calculateTF(term, document)                // 计算词项频率
+	idf := calculateIDF(term, documents)             // 计算逆文档频率
+	doclen := float64(len(strings.Fields(document))) // 文档长度（单词数）
+
+	corpusSize := float64(len(documents)) // 文档集合大小
 	df := 0.0
 	for _, doc := range documents {
 		if strings.Contains(doc, term) {
 			df++ // 统计包含给定词项的文档数
 		}
 	}
-	//实际应用时,取自然对数，可以使得idf的取值范围更广泛，更好地表征词项的稀有程度。
 	//log := math.Log((corpusSize - df + 0.5) / (df + 0.5))
-	//bm25 := (idf * tf * (k1 + 1)) / (tf + k1*(1-b+b*(doclen/avgdl))) * log
 	bm25 := (idf * tf * (k1 + 1)) / (tf + k1*(1-b+b*(doclen/avgdl))) * ((corpusSize - df + 0.5) / (df + 0.5)) // 计算 BM25 分数
 	return bm25
 }
@@ -54,4 +54,43 @@ func calculateAverageDocumentLength(documents []string) float64 {
 		totalLength += len(strings.Fields(doc)) // 统计单词数
 	}
 	return float64(totalLength) / float64(len(documents)) // 返回平均文档长度
+}
+
+// 获取分数最高的前 k 个文档的索引
+func getTopKDocumentIndices(scores []float64, k int) []int {
+	docIndices := make([]int, len(scores))
+	for i := range docIndices {
+		docIndices[i] = i // 初始化索引数组
+	}
+
+	// 按分数降序排序索引
+	sort.SliceStable(docIndices, func(i, j int) bool {
+		return scores[docIndices[i]] > scores[docIndices[j]]
+	})
+
+	return docIndices[:k] // 返回前 k 个文档的索引
+}
+
+// 并行计算所有文档的BM25分数
+func CalculateBM25Scores(query string, documents []string, avgdl float64, k1 float64, b float64) []float64 {
+	scores := make([]float64, len(documents))
+	var wg sync.WaitGroup
+	wg.Add(len(documents))
+
+	ch := make(chan int)
+	go func() {
+		for i := 0; i < len(documents); i++ {
+			idx := <-ch
+			scores[idx] = calculateBM25(query, documents[idx], documents, avgdl, k1, b)
+			wg.Done()
+		}
+	}()
+
+	for i := 0; i < len(documents); i++ {
+		ch <- i
+	}
+	wg.Wait()
+	close(ch)
+
+	return scores
 }
