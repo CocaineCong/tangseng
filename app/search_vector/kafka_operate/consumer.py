be1af8a36e5eb4da0213b06760c2598819c2b42d
FanOne <294350394@qq.com>
2023-11-03 00:07:23 +0800

    fix: sonar

--- app/search_vector/kafka_operate/consumer.py
+++ app/search_vector/kafka_operate/consumer.py
@@ -8,14 +8,10 @@ def consumer_inverted_index():
     kafka_helper = KafkaHelper(kafka_servers)
     kafka_helper.connect_consumer(kafka_topic)
     kafka_helper.consume_messages()
-    
-
 
 # 创建线程对象
 thread = threading.Thread(target=consumer_inverted_index)
-
 # 启动线程
 thread.start()
-
 # 等待线程结束
 thread.join()
