78f9507df0413f97710f1acf3ca24902c4eebcc6
FanOne <294350394@qq.com>
2025-05-10 11:14:54 +0800

    feat:refactor bloom filter

--- pkg/bloom_filter/bloom_fillter_test.go
+++ pkg/bloom_filter/bloom_fillter_test.go
@@ -18,20 +18,20 @@
 package bloom_filter
 
 import (
-	"fmt"
 	"testing"
+
+	"github.com/go-playground/assert/v2"
 )
 
 func TestBloomFilter(t *testing.T) {
-	bf := NewBloomFilter(1000, 0.01)
+	bf := NewBloomFilter(WithNumber(1000), WithFalseRate(0.01))
 
 	bf.Add("呀哈哈哈")
-	bf.Add("国王资料")
+	bf.Add("塞尔达")
 	bf.Add("国王之泪")
-
-	fmt.Println(bf.Contains("国王"))         // false
-	fmt.Println(bf.Contains("哈哈"))         // false
-	fmt.Println(bf.Contains("国王之泪"))       // true
-	fmt.Println(bf.Contains("durian"))     // false
-	fmt.Println(bf.Contains("elderberry")) // false
+	assert.IsEqual(bf.Contains("国王"), false)
+	assert.IsEqual(bf.Contains("哈哈"), false)
+	assert.IsEqual(bf.Contains("国王之泪"), true)
+	assert.IsEqual(bf.Contains("赛尔"), false)
+	assert.IsEqual(bf.Contains("塞尔达"), true)
 }
